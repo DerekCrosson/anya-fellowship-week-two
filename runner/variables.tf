a87f280372194ff194200cd982f9324dbe0ec0bd
Derek Crosson <derekcrosson18@gmail.com>
2022-05-30 04:04:03 +0200

    feat: create runner

---
+++ runner/variables.tf
@@ -0,0 +1,18 @@
+variable "PREFIX" {
+    default = "ecs-runner"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR for the VPC"
+    default = "20.0.0.0/16"
+}
+
+variable "private_subnet_cidr" {
+    description = "CIDR for the Private Subnet"
+    default = "20.0.0.0/24"
+}
+
+variable "public_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "20.0.255.0/24"
+}
